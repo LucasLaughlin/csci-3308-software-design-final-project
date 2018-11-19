1d4f62f704271e55f7e3567114c8bd17e6cca67a
Yushen Chang <17364372+Yushen-Chang-9@users.noreply.github.com>
2018-11-18 18:44:36 -0700

    2nd push

--- src/sensor_sql.py
+++ src/sensor_sql.py
@@ -1,101 +1,107 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import postgresql
 
+import sqlite3
 import os
 import time
-import glob
-
-# global variables
-speriod=(15*60)-1
-dbname='/var/www/templog.db'
 
 
-
-# store the temperature in the database
-def log_temperature(temp):
-
-    conn=postgresql.connect(dbname)
-    curs=conn.cursor()
-
-    curs.execute("INSERT INTO temps values(datetime('now'), (?))", (temp,))
-
-    # commit the changes
-    conn.commit()
-
-    conn.close()
+#csci3308 milestone 4 script
+#This script is used on the raspberry pi 3 B+ to get temperature data and store it
+#in a sql database that is also on the raspberry pi 3 B+.
+#The name of the sensor device file: #28-00000a3cdc2c
+#Device File Path:  /sys/bus/w1/devices/28-00000a3cdc2c/w1_slave
 
 
-# display the contents of sql
-def display_data():
+#Global Variables
+#sampling frequency for sensor:
+sample = (5*60)-1
+#database object to store at sql path: /var/wwww/templog.db
+myDatabase = '/var/www/templog.db'
+#devive file
+#device_file = '28-00000a3cdc2c'
+#Function to measure temperature from device file for DS18B20 sensor
 
-    conn=postgresql.connect(dbname)
-    curs=conn.cursor()
-
-    for row in curs.execute("SELECT * FROM temps"):
-        print str(row[0])+"	"+str(row[1])
-
-    conn.close()
-
-
-
-# get temerature
-# returns None on error, or the temperature as a float
-def get_temp(devicefile):
+def getData(devicefile):
 
     try:
-        fileobj = open(devicefile,'r')
-        lines = fileobj.readlines()
-        fileobj.close()
+
+        data_object = open(devicefile, 'r')
+        lines = myDatabase.readlines()
+        data_object.close()
+
     except:
+
         return None
 
-    # get the status from the end of line 1 
+
+    #Display status
     status = lines[0][-4:-1]
 
-    # is the status is ok, get the temperature from line 2
-    if status=="YES":
+    #if sensor data reading is succesful
+    if status == "Device is reading..." :
+
         print status
-        tempstr= lines[1][-6:-1]
-        tempvalue=float(tempstr)/1000
-        print tempvalue
-        return tempvalue
+        store_data = line[1][-6:-1]
+
+
+         data_value = float(store_data)/1000
+        print data_value
+        return data_value
+    #else the device encountered an issue
     else:
-        print "There was an error."
+
+        print "Error: Device has encountered an issue "
         return None
 
+#storing the sensor data in the sql databse or any database
+def storage(temp):
+
+    #connect class is used to connect to sql database also used for socket programming
+    db_connect = sqlite3.connect(myDatabase)
+    #cursor class allows Python to execute PostgreSQL command in database session
+    db_cursor = db_connect.cursor()
+    db_cursor.execute("INSERT INTO temps values(datetime('now'), (?))", (temp,))
+
+    #commit changes to database SQL
+    db_connect.commit()
+    #terminate connection to database SQL
+    db_connect.close()
+
+#display database
+def display():
+
+    db_connect = sqlite3.connect(myDatabase)
+    db_cursor = db_connect.cursor()
+
+    #iterate through contents of db
+    for row in db_cursor.execute("SELECT * FROM temps"):
+        print str(row[0]) + "  " + str(row[1])
+        db_connect.close()
+
+
+
+#main
 def main():
 
-    # enable kernel modules
+    #enable kernel modules
     os.system('sudo modprobe w1-gpio')
     os.system('sudo modprobe w1-therm')
 
-   
-    #The raspberry pi temperature sensor address to read
+    #search for sensor device path (cd /sys/bus/w1/devices/28*) to device address
     devicelist = glob.glob('/sys/bus/w1/devices/28*')
-    if devicelist=='':
+    if devicelist == '':
         return None
     else:
-        # append /w1slave to the device file
-        w1devicefile = devicelist[0] + '/w1_slave'
+        #append /w1_slave to device file path
+        w1_devicefile = devicelist[0] + 'w1_slave'
 
-
-#    while True:
-
-    # get the temperature from the device file
-    temperature = get_temp(w1devicefile)
+    #while true
+    #call getData function from devicefile
+    temperature = getData(w1_devicefile)
     if temperature != None:
-        print "temperature="+str(temperature)
-    else:
-       
-        temperature = get_temp(w1devicefile)
-        print "temperature="+str(temperature)
+        print "temperature = "+str(temperature)
 
-        # Store the temperature in the database
-    log_temperature(temperature)
-
-
-
-
-if __name__=="__main__":
-    main()
+    #store meausured temperature into sql database
+    display()
+    #time.sleep(meausre)
