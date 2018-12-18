376e39eb21c0d0bc3a1f9bf61a7e56906f364561
achamorr <andrea.colleges@gmail.com>
2018-12-18 11:17:33 -0700

    updated folder names

--- unit-testing/test_chrome.py
+++ unit-testing/test_chrome.py
@@ -4,13 +4,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 import time
-import os
+from selenium.webdriver.common import service
 
 
 class BasicSearch(unittest.TestCase):
 
     def setUp(self):
+        #srv = service.Service('/usr/bin/chromedriver.exe')
+        #srv.start()
+        #capabilities = {'chrome.binary': '/usr/bin/google-chrome'}
+        #self.driver = webdriver.Remote(srv.service_url, capabilities)
+        #self.driver = webdriver.Chrome(executable_path= 'C:/usr/bin/chromedriver.exe')
         self.driver = webdriver.Chrome()
+        #driver.implicitly_wait(30)
+
 
 
     def test_button_dropdown(self):
